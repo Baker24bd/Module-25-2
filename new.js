31965416ba5d61f1350b2128a66ebcde887e6ab1
Baker24bd <mdbakerbdn@gmail.com>
2021-02-05 18:54:19 +0600

    work

--- new.js
+++ new.js
@@ -8,3 +8,14 @@ class Parson {
 
 const heroParson = new Parson('Hero', 'Alom', 25000);
 console.log(heroParson);
+const friendsParson = new Parson('Hero', 'kalam', 20000);
+console.log(friendsParson);
+
+function Parson1(firsName, lastName, salary){
+    this.firsName = firsName;
+    this.lastName = lastName;
+    this.salary = salary;
+}
+
+const oldParson = new Parson1('Niha', 'AKter', 1200);
+console.log(oldParson);
